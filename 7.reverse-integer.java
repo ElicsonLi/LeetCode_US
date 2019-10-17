317ac4c055148858f0e2e500683e7535a1a97b43
Zhengpei Li <elicsonli@qq.com>
2019-10-17 12:46:14 +0800

    no need to consider negetive number as a speical

--- 7.reverse-integer.java
+++ 7.reverse-integer.java
@@ -7,35 +7,26 @@
 // @lc code=start
 class Solution {
     public int reverse(int x) {
-        // boolean fuhao = true;
-        // long ans = 0;
-        // if( x < 0){
-        //     fuhao = false;
-        //     x = -1 * x;
-        // }
-        // x = Math.abs(x);
-        // do{
-        //      int y = x % 10;
-        //      ans = ans * 10 + y;
-        //      x /= 10; 
-        // }while(x != 0);
+        boolean fuhao = true;
+        long ans = 0;
+        if( x < 0){
+            fuhao = false;
+            x = -1 * x;
+        }
+        x = Math.abs(x);
+        do{
+             int y = x % 10;
+             ans = ans * 10 + y;
+             x /= 10; 
+        }while(x != 0);
         
-        // if(ans > Integer.MAX_VALUE || ans < Integer.MIN_VALUE ){
-        //     return 0;
-        // }else if(fuhao){
-        //     return (int) ans;
-        // }else{
-        //     return (int) (-1 * ans);
-        // }
-        long r = 0;
-		while(x != 0){
-			r = r*10 + x%10;
-			x /= 10;
-		}
-		if(r >= Integer.MIN_VALUE && r <= Integer.MAX_VALUE)
-			return (int)r;
-		else
-			return 0;
+        if(ans > Integer.MAX_VALUE || ans < Integer.MIN_VALUE ){
+            return 0;
+        }else if(fuhao){
+            return (int) ans;
+        }else{
+            return (int) (-1 * ans);
+        }
 
     }
 }

7d4bffbf8851d35cec069441fff06f511e55ed2c
Zhengpei Li <elicsonli@qq.com>
2019-12-01 21:09:02 +0800

    finish 405

---
+++ 405.convert-a-number-to-hexadecimal.java
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode id=405 lang=java
+ *
+ * [405] Convert a Number to Hexadecimal
+ */
+
+// @lc code=start
+class Solution {
+    public String toHex(int num) {
+        return Integer.toHexString(num);
+
+        
+    }
+}
+// @lc code=end
+
